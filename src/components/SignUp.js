5100a19d1775eda337ad655416e9d164d51a75c9
Carlos Martinez <martinezcj2@gmail.com>
2019-03-23 02:38:41 -0400

    Implement sign up with firebase

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+import firebase from '../firebase';
 
 class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    error: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(response => {
+        console.log('sign up response', response);
+      })
+      .catch(err => {
+        const { message } = err;
+        
+        this.setState({ error: message });
+      });
+  }
+
+  componentDidMount() {
+    this.unsubcribed = firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.props.history.push('/');
+      }
+      else {
+        // TODO: What would go here?
+      }
+    });
+  }
+
+  componentDidUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
+    const { email, password, error } = this.state;
+    const displayError = error !== '' ?  <div>{error}</div> : '';
+
     return (
       <div>
-        This is the signup Component
+        <hr/>
+          <form>
+            <input type='email' name='email' value={email} onChange={this.handleChange} placeholder='email' />
+            <input type='password' name='password' value={password} onChange={this.handleChange} placeholder='password' />
+            <button onClick={this.handleSubmit}>sign up</button>
+          </form>
+          { displayError }
+        <hr/>
       </div>
     );
   }
