bb9b4892563a82f573c2049ec9b3c8c8b6dffb19
Carlos Martinez <martinezcj2@gmail.com>
2019-03-23 17:49:24 -0400

    Add firebase user check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
+import firebase from './firebase';
+
+import AuthContext from './contexts/auth';
 import SignUp from './containers/SignUp';
 import LogIn from './containers/LogIn';
 import LogOut from './containers/LogOut';
@@ -8,16 +11,37 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 class App extends Component {
+  state = { 
+    user: ''
+  }
+
+  componentDidMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setState({ user });
+      }
+      else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  
   render() {
     return (
       <HashRouter>
-        <>
-          <Route path='/' component={ Navbar } />
-          <Route path='/' exact component={ Home } />
-          <Route path='/signup' component={ SignUp } />
-          <Route path='/login' component={ LogIn } />
-          <Route path='/logout' component={ LogOut } />
-        </>
+        <AuthContext.Provider value={this.state.user}>
+          <>
+            <Route path='/' component={ Navbar } />
+            <Route path='/' exact component={ Home } />
+            <Route path='/signup' component={ SignUp } />
+            <Route path='/login' component={ LogIn } />
+            <Route path='/logout' component={ LogOut } />
+          </>
+        </AuthContext.Provider>
       </HashRouter>
     );
   }
