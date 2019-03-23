3dc3574aefcef11acb53ecdf4110bd0c63388f95
Carlos Martinez <martinezcj2@gmail.com>
2019-03-23 02:04:50 -0400

    Create components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+  );
+};
+
+export default Navbar;
