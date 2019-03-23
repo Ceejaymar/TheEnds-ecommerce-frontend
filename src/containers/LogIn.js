f1948fda6319354a376415a5909a3fc42fde249a
Carlos Martinez <martinezcj2@gmail.com>
2019-03-23 17:52:35 -0400

    Remove componentDidMount & componentWillUnmount & import auth context

--- src/containers/LogIn.js
+++ src/containers/LogIn.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import firebase from '../firebase';
+import AuthContext from '../contexts/auth';
 
 class LogIn extends Component {
   state = {
@@ -23,42 +25,41 @@ class LogIn extends Component {
       })
       .catch(err => {
         const { message } = err;
-        
+
         this.setState({ error: message });
       });
   }
 
-  componentDidMount() {
-    this.unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        this.props.history.push('/');
-      }
-      else {
-        // ? What would go here?
-      };
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
   render() {
     const { email, password, error } = this.state;
-    const displayError = error !== '' ?  <div>{error}</div> : '';
+    const displayError = error !== '' ? <div>{error}</div> : '';
+    const displayForm = (
+      <div>
+        <hr />
+        <h2>Log In</h2>
+        <form>
+          <input type='email' name='email' value={email} onChange={this.handleChange} placeholder='email' />
+          <input type='password' name='password' value={password} onChange={this.handleChange} placeholder='password' />
+          <button onClick={this.handleSubmit}>log in</button>
+        </form>
+        {displayError}
+        <hr />
+      </div>
+    )
 
     return (
-      <div>
-        <hr/>
-          <h2>Log In</h2>
-          <form>
-            <input type='email' name='email' value={email} onChange={this.handleChange} placeholder='email' />
-            <input type='password' name='password' value={password} onChange={this.handleChange} placeholder='password' />
-            <button onClick={this.handleSubmit}>log in</button>
-          </form>
-          { displayError }
-        <hr/>
-      </div>
+      <AuthContext.Consumer>
+        {
+          user => {
+            if (user) {
+              return <Redirect to='/' />
+            } 
+            else {
+              return displayForm;
+            }
+          }
+        }
+      </AuthContext.Consumer>
     );
   }
 };
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import firebase from '../firebase';
+import AuthContext from '../contexts/auth';
 
 class SignUp extends Component {
   state = {
@@ -23,44 +25,43 @@ class SignUp extends Component {
       })
       .catch(err => {
         const { message } = err;
-        
+
         this.setState({ error: message });
       });
   }
 
-  componentDidMount() {
-    this.unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        this.props.history.push('/');
-      }
-      else {
-        // ? What would go here?
-      };
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
   render() {
     const { email, password, error } = this.state;
-    const displayError = error !== '' ?  <div>{error}</div> : '';
-
-    return (
+    const displayError = error !== '' ? <div>{error}</div> : '';
+    const displayForm = (
       <div>
-        <hr/>
-          <h2>Sign Up</h2>
-          <form>
-            <input type='email' name='email' value={email} onChange={this.handleChange} placeholder='email' />
-            <input type='password' name='password' value={password} onChange={this.handleChange} placeholder='password' />
-            <button onClick={this.handleSubmit}>sign up</button>
-          </form>
-          { displayError }
-        <hr/>
+        <hr />
+        <h2>Sign Up</h2>
+        <form>
+          <input type='email' name='email' value={email} onChange={this.handleChange} placeholder='email' />
+          <input type='password' name='password' value={password} onChange={this.handleChange} placeholder='password' />
+          <button onClick={this.handleSubmit}>sign up</button>
+        </form>
+        {displayError}
+        <hr />
       </div>
     );
+
+    return (
+      <AuthContext.Consumer>
+        {
+          user => {
+            if (user) {
+              return <Redirect to='/' />
+            }
+            else {
+              return displayForm;
+            }
+          }
+        }
+      </AuthContext.Consumer>
+    );
   }
-};
+}
 
 export default SignUp; 
