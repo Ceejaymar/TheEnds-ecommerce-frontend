9ca4a4806270082c32686de056bd1b66c027be42
Carlos Martinez <martinezcj2@gmail.com>
2019-03-24 03:21:12 -0400

    Get logged in user info & display on page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import firebase from '../firebase';
 
 class Home extends Component {
+  state = {
+    email: '',
+    uid: '',
+    token: ''
+  }
+
+  componentDidMount() {
+    this.unsubscribed = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ email: user.email, uid: user.uid })
+      };
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribed();
+  }
+  
   render() {
-    return (
-      <div>
-        this is the home Component.
-      </div>
-    );
+    const { email, uid } = this.state;
+
+    if (!email) {
+      return <h1>Please log in!</h1>
+    }
+    else {
+      return (
+        <>
+          <div>
+            <h2>Welcome back, { email }</h2>
+            <h4>Your user id is: { uid }</h4>
+          </div>
+        </>
+      );
+    }
   }
 }
 
