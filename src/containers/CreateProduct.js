da426b186378ab4d599743de0ee57684bd678f6f
Carlos Martinez <martinezcj2@gmail.com>
2019-04-07 13:53:51 -0400

    Start building out createProduct component

--- src/containers/CreateProduct.js
+++ src/containers/CreateProduct.js
@@ -20,11 +20,47 @@ class CreateProduct extends Component {
     }
   }
 
+  handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div>
-        <input type='file' accept='image/*' onChange={this.handleFileInput} />
         <label>Upload Image</label>
+        <input type='file' accept='image/*' onChange={this.handleFileInput} />
+        <hr/>
+        <label>Product Name</label>
+        <input type='text' name='name' />
+        <hr/>
+        <label>Product Price</label>
+        <input 
+          type='number' 
+          min='0.00' 
+          step='0.50' 
+          max='3000' 
+          name='price' 
+          onChange={this.handleChange} 
+          placeholder='price'
+        />
+        <hr/>
+        <label>Category</label>
+        <select name="category" id="">
+          <option value="">Tops</option>
+          <option value="">Bottoms</option>
+          <option value="">Accessories</option>
+        </select>
+        <hr/>
+        <label>Description</label>
+        <textarea name="" id="" cols="30" rows="10" placeholder="Write product description here"></textarea>
+        <hr/>
+        <label>Stock</label>
+        <p>Does your product have sizes?</p>
+        <label className="switch">
+          <input type='checkbox' />
+          <span className="slider round"></span>
+        </label>
+        <input type='number' placeholder="stock amount" />
       </div>
     );
   }
