931a0175264e4d48ddf14008921f50dd2c6a03a1
Carlos Martinez <martinezcj2@gmail.com>
2019-04-09 03:10:44 -0400

    COmment out console log

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import firebase from '../firebase';
 import AuthContext from '../context/auth';
 
 class Home extends Component {
-  
   static contextType = AuthContext;
 
   state = {
@@ -13,7 +11,6 @@ class Home extends Component {
     token: ''
   }
 
-  // dont know why the token, isn't getting updated. It started working for a little bit. Not sure why it started working or why it stopped.
   componentDidMount() {
     if (this.context) {
       console.log('this is the context in home', this.context);
@@ -33,9 +30,8 @@ class Home extends Component {
       })
   }
 
-  // Fam this don't work
   handlePrivateRoute = () => {
-    console.log('the token is here famm', this.state.token);
+    // console.log('the token is here famm', this.state.token);
     axios.get('http://localhost:3000/user/1/protected', { token: this.state.token })
       .then(data => {
         console.log(data)
@@ -48,41 +44,34 @@ class Home extends Component {
   handleState = (user) => {
     this.setState({ email: user.email });
   }
-  
-  render() {
-    const { email, uid } = this.state;
-    // const displayLinks = (
-    //   <div>
-    //     hello
-    //   </div>
-    // )
-      // console.log('this is the context', this.context);
-      console.log('this is the state in HOME', this.state);
 
-      return (
-        <>          
-          <AuthContext.Consumer>
-          {
-            state => {
-              if (state.user) {
-                return (
-                  <>
-                    <div>hello</div>
-                    <h2>Welcome back, { state.email }</h2>
-                    <h4>Your user id is: { state.uid }</h4>
-                    <button onClick={this.handlePublicRoute}>click for the public route</button>
-                    <button onClick={this.handlePrivateRoute}>click for the private route</button>
-                  </>
-                )
-              }
-              else {
-                return <h1>Please log in!</h1>
-              }
+  render() {
+    console.log('this is the state in HOME', this.state);
+
+    return (
+      <>          
+        <AuthContext.Consumer>
+        {
+          state => {
+            if (state.user) {
+              return (
+                <>
+                  <div>hello</div>
+                  <h2>Welcome back, { state.email }</h2>
+                  <h4>Your user id is: { state.uid }</h4>
+                  <button onClick={this.handlePublicRoute}>click for the public route</button>
+                  <button onClick={this.handlePrivateRoute}>click for the private route</button>
+                </>
+              )
+            }
+            else {
+              return <h1>Please log in!</h1>
             }
           }
-        </AuthContext.Consumer>
-      </>
-      );
+        }
+      </AuthContext.Consumer>
+    </>
+    );
     }
   
 }
