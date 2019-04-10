28c27b8bf286e9343f6f77f318a93eacf6dddec4
Carlos Martinez <martinezcj2@gmail.com>
2019-04-10 02:14:58 -0400

    Add routes for Stores and Store containers

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+// Context
 import AuthContext from './context/auth';
+
+// Containers
 import SignUp from './containers/SignUp';
 import LogIn from './containers/LogIn';
 import LogOut from './containers/LogOut';
+import Stores from './containers/Stores';
+import Store from './containers/Store';
+import CreateProduct from './containers/CreateProduct';
+
+// Components 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import CreateProduct from './containers/CreateProduct';
 
 const Routes = ({ state }) => {
   return (
@@ -20,6 +27,8 @@ const Routes = ({ state }) => {
             <Route path='/signup' component={SignUp} />
             <Route path='/login' component={LogIn} />
             <Route path='/logout' component={LogOut} />
+            <Route path='/stores' component={Stores} />
+            <Route path='/store/:id' component={Store} />
             <Route path='/createproduct' component={CreateProduct} />
           </Switch>
         </>
