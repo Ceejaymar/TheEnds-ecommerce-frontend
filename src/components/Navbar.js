b0d24aade4d65f0600e1a58785a7c78ccccd3c64
Carlos Martinez <martinezcj2@gmail.com>
2019-04-10 02:15:54 -0400

    Add Link to Store && Brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,22 +10,30 @@ const Navbar = () => {
           if (state.user) {
             return (
               <>
+                <div>The Ends</div>
                 <Link to='/'>Home</Link>
+                <br />
+                <Link to='/stores'>Marketplace</Link>
                 <br />
                 <Link to='/'>Account</Link>
                 <br />
                 <Link to='/logout'>Log out</Link>
+                <hr />
               </>
             )
           }
           else {
             return (
               <>
+                <div>The Ends</div>
                 <Link to='/'>Home</Link>
+                <br/>
+                <Link to='/stores'>Marketplace</Link>
                 <br/>
                 <Link to="/signup">Sign Up</Link>
                 <br />
                 <Link to="/login">Log In</Link>
+                <hr />
               </>
             )
           };
