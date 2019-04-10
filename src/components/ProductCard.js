ddabee3037183103cd94bdd8eca81c90039ff540
Carlos Martinez <martinezcj2@gmail.com>
2019-04-10 17:41:48 -0400

    Add Link to around card content

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductCard = ({ productInfo }) => {
-  const { name, url, price } = productInfo;
-  
+  const { name, url, price, id } = productInfo;
+
   return (
     <div>
-      <h3>{ name }</h3>
-      <img style={{ "width": "200px" }} src={url} alt="product" />
-      <p>{ price }</p>
+      <Link to={`/product/${id}`}>
+        <h3>{ name }</h3>
+        <img style={{ "width": "200px" }} src={ url } alt="product" />
+        <p>{ price }</p>
+      </Link>
     </div>
   );
 };
