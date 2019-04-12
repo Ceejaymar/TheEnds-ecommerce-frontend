dcf87a4aebee5731f1543cd9085810fb284b75a6
Carlos Martinez <martinezcj2@gmail.com>
2019-04-12 03:55:44 -0400

    Create Serice file for localStorage

---
+++ src/services.js
@@ -0,0 +1,26 @@
+const CartService = {};
+
+CartService.init = () => {
+  if(localStorage.getItem('cart') === null) {
+    localStorage.setItem('cart',  JSON.stringify([]));
+  }
+}
+
+CartService.getCart = () => {
+  const CartArrayString = localStorage.getItem('cart');
+  const CartArray = JSON.parse(CartArrayString);
+
+  return CartArray;
+}
+
+CartService.saveCart = (cartItems, user, timestamp) => {
+    const newCart = { cartItems, timestamp };
+    const oldCarts = CartService.getItems();
+
+    // oldCarts.unshift(newCart);
+
+    localStorage.setItem('cart', JSON.stringify(oldCarts));
+
+    return oldCarts;
+}
+
