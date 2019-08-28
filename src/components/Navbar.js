e496a66a65d8ed83cbb2ca343b245f7d25700282
Carlos Martinez <martinezcj2@gmail.com>
2019-08-27 20:45:47 -0400

    Create Navbar styles file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
 import AuthContext from '../context/auth';
 
-const Navbar = () => {
+import styles from '../styles/NavbarStyles';
+
+
+const Navbar = ({ classes }) => {
   return (
     <AuthContext.Consumer>
       {
         state => {
           if (state.user) {
             return (
-              <>
-                <div>The Ends</div>
-                <Link to='/'>Home</Link>
-                <br />
-                <Link to='/marketplace'>Marketplace</Link>
-                <p>Welcome back, { state.user.email }</p>
-                <Link to='/createproduct'>
-                  <i className="icon ion-md-add-circle"></i>
-                </Link>
-                <br />
-                <Link to='/cart'>
-                  <i className="icon ion-md-cart"></i>
-                </Link>
-                <br />
-                <Link to='/account'>
-                  <i className="icon ion-md-person"></i>
-                </Link>
-                <br />
-                <Link to='/logout'>Log out</Link>
-                <hr />
-              </>
+              <div className={classes.nav}>
+                <div className={classes.brand}>The Ends</div>
+                <div classname={classes.menu}>
+                  <div classname={classes.navLinks}>
+                    <Link className={classes.link} to='/'>Home</Link>
+                    <Link className={classes.link} to='/marketplace'>Marketplace</Link>
+                    <Link className={classes.link} to='/about'>About</Link>
+                  </div>
+                  <div>
+                    <p>Welcome back, { state.user.email }</p>
+                    <Link className={classes.link} to='/createproduct'>
+                      <i className="icon ion-md-add-circle"></i>
+                    </Link>
+                    <Link className={classes.link} to='/cart'>
+                      <i className="icon ion-md-cart"></i>
+                    </Link>
+                    <Link className={classes.link} to='/account'>
+                      <i className="icon ion-md-person"></i>
+                    </Link>
+                    <Link className={classes.link} to='/logout'>Log out</Link>
+                  </div>
+                </div>
+              </div>
             )
           }
           else {
             return (
-              <>
-                <div>The Ends</div>
-                <Link to='/'>Home</Link>
-                <br/>
-                <Link to='/marketplace'>Marketplace</Link>
-                <br/>
-                <Link to="/signup">Sign Up</Link>
-                <br />
-                <Link to="/login">Log In</Link>
-                <hr />
-              </>
+              <div className={classes.nav}>
+                <div className={classes.brand}>The Ends</div>
+                <div className={classes.menu}>
+                  <div className={classes.navLinks}>
+                    <Link className={classes.link} to='/'>Home</Link>
+                    <Link className={classes.link} to='/marketplace'>Marketplace</Link>
+                    <Link className={classes.link} to='/about'>About</Link>
+                  </div>
+                  <div>
+                    <Link className={classes.link} to="/signup">Sign Up</Link>
+                    <Link className={classes.link} to="/login">Log In</Link>
+                  </div>
+                </div>
+                {/* <hr /> */}
+              </div>
             )
           };
         }
@@ -53,4 +64,4 @@ const Navbar = () => {
   );
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
