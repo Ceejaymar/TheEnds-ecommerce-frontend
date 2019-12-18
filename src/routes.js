03ea2e3d0031594d1fab056958c1f11c6d9d1de6
Ceejaymar <martinezcj2@gmail.com>
2019-12-18 03:41:15 -0500

    Remove unused import

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 // Context
 import { AuthProvider } from './context/auth';
