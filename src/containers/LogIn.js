376ab5969116903ae011f7e526eb2534006e69c0
Ceejaymar <martinezcj2@gmail.com>
2019-12-27 17:47:05 -0500

    Refactor to hooks

--- src/containers/LogIn.js
+++ src/containers/LogIn.js
@@ -1,68 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import firebase from '../firebase';
 import { AuthContext } from '../context/auth';
+import useInputStateHook from '../hooks/useInputStateHook';
 
-class LogIn extends Component {
-  constructor(props) {
-    super(props);
+function LogIn() {
+  const [email, setEmail] = useInputStateHook('');
+  const [password, setPassword] = useInputStateHook('');
+  const [error, setError] = useState('');
 
-    this.state = {
-      email: '',
-      password: '',
-      error: '',
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleSubmit = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    const { email, password } = this.state;
     try {
       const response = await firebase.auth().signInWithEmailAndPassword(email, password);
       // eslint-disable-next-line no-console
       console.log('sign up response', response);
-    } catch (error) {
-      this.setState({ error: error.message });
+    } catch (err) {
+      setError(err.message);
     }
   }
 
-  render() {
-    const { email, password, error } = this.state;
-    const displayError = error !== '' ? <div>{error}</div> : '';
-    const displayForm = (
-      <div>
-        <hr />
-        <h2>Log In</h2>
-        <form>
-          <input type="email" name="email" value={email} onChange={this.handleChange} placeholder="email" />
-          <input type="password" name="password" value={password} onChange={this.handleChange} placeholder="password" />
-          <button type="button" onClick={this.handleSubmit}>log in</button>
-        </form>
-        {displayError}
-        <hr />
-      </div>
-    );
+  const displayError = error !== '' ? <div>{error}</div> : '';
+  const displayForm = (
+    <div>
+      <hr />
+      <h2>Log In</h2>
+      <form>
+        <input type="email" name="email" value={email} onChange={setEmail} placeholder="email" />
+        <input type="password" name="password" value={password} onChange={setPassword} placeholder="password" />
+        <button type="button" onClick={handleSubmit}>log in</button>
+      </form>
+      {displayError}
+      <hr />
+    </div>
+  );
 
-    return (
-      <AuthContext.Consumer>
-        {
-          (state) => {
-            // eslint-disable-next-line no-console
-            console.log('user in login', state);
-            if (state.user) {
-              return <Redirect to="/" />;
-            }
-            return displayForm;
+  return (
+    <AuthContext.Consumer>
+      {
+        (state) => {
+          // eslint-disable-next-line no-console
+          console.log('user in login', state);
+          if (state.user) {
+            return <Redirect to="/" />;
           }
+          return displayForm;
         }
-      </AuthContext.Consumer>
-    );
-  }
+      }
+    </AuthContext.Consumer>
+  );
 }
 
 export default LogIn;
