9bcbd33c4c42f214d9472501ff20fbd976d18258
Ceejaymar <martinezcj2@gmail.com>
2020-01-19 03:46:04 -0500

    Add styling to login

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,10 @@ const About = () => (
       <img className="About__img" src="https://images.unsplash.com/photo-1530884698386-d42ad3199b1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80" alt="stylish man on sand" />
     </figure>
     <div>
-      the about section
+      <h3>Lorem ipsum, dolor sit amet</h3>
+      <h2>Lorem ipsum dolor, sit amet consectetur adipisicing </h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore a, autem nulla rem magni ab. Ratione modi nemo minus unde, amet, deserunt illo debitis quae sit iste, dolorem nesciunt optio!</p>
+      <button>To Marketplace</button>
     </div>
   </div>
 );
